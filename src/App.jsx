c222df5e82fad3a0ea9b4d05cc355d34b300741f
BahirYasrebSeemab <bahiryasreb@gmail.com>
2026-03-17 15:33:40 +0500

    changed file package manager from npm to bun, installed react-router-dom and initialized it to a file and passed it to main, initialized the feature based file structure where each feature will git its own hooks components and pages and each one has its own logic and ecosystem without interveing other features, created shared directory for shared logic and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
-import {
-    QueryClient,
-    QueryClientProvider
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <QueryClientProvider client={queryClient}>
-      <h1>Hello There</h1>  
-  </QueryClientProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
