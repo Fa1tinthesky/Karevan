a16c4ed0c0d395ea286733a59e175ea3b86c0250
Fa1tinthesky <fate.public@gmail.com>
2026-03-23 01:30:21 +0500

    Integrated loveable's front-end

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-      react(),
-      tailwindcss(),
-  ],
+    react(),
+    tailwindcss(),,
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
